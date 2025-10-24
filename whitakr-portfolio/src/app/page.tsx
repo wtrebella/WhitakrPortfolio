281bc9ee2c487098771545a301170493c3007600
Whitaker Trebella <wtrebella@gmail.com>
2025-10-23 19:12:45 -0700

    Initial project commit

---
+++ whitakr-portfolio/src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+	return (
+		<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+  			<main>
+				<h1>
+					I'm Whit
+				</h1>
+				<h2>
+					Call Me Whit
+				</h2>
+			</main>
+		</div>
+	);
+}
