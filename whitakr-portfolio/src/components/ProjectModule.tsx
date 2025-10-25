7586219a919c3b98b34d02a30320c82a3a71fa9f
Whitaker Trebella <wtrebella@gmail.com>
2025-10-25 13:34:35 -0700

    Changed look of project module

--- whitakr-portfolio/src/components/ProjectModule.tsx
+++ whitakr-portfolio/src/components/ProjectModule.tsx
@@ -7,7 +7,7 @@ export default function ProjectModule(
 	const trimmedLink = link?.trim();
 	const linkProps = getLinkProps(trimmedLink);
 	return (
-        <div className="flex flex-row mt-8 p-4 border rounded-lg w-full max-w-2xl">
+        <div className="flex flex-row mt-8 p-4 rounded-lg w-full max-w-2xl shadow-md bg-neutral-800">
             <div className="flex flex-col p-4 min-w-[30dvh]">
                 <div className="flex flex-col items-start justify-center p-4">
                     <h1 className="text-2xl">{title}</h1>
