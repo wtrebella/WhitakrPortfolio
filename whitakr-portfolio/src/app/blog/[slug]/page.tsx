e52e91d20e85f10fa1f81bb734f5947a2949107c
Whitaker Trebella <wtrebella@gmail.com>
2025-10-24 19:21:05 -0700

    Gave the blog slug a base page too

---
+++ whitakr-portfolio/src/app/blog/[slug]/page.tsx
@@ -0,0 +1,8 @@
+export default function BlogSlug() {
+    return (
+        <div className="min-h-full flex flex-col items-center justify-center p-4">
+            <h2>Blog</h2>
+            <p>Welcome to my blog slug!</p>
+        </div>
+    );
+}
