649c7a496f74f6882d166225755af81ad4f2aa8e
Whitaker Trebella <wtrebella@gmail.com>
2025-10-24 19:27:29 -0700

    Fixed more className issues

--- whitakr-portfolio/src/components/Footer.tsx
+++ whitakr-portfolio/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
 	return (
-		<footer className="mt-8">
-			<p className>Here's the footer with some various info that may be important or may not be</p>
+		<footer className="m-4 flex flex-col items-center justify-center">
+			<p className="text-sm text-slate-500">Here's the footer with some various info that may be important or may not be</p>
 		</footer>
 	);
 }
