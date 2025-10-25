10b94a28ae26881b92e8ba2b10f354d52e038e75
Whitaker Trebella <wtrebella@gmail.com>
2025-10-24 19:23:01 -0700

    Fixed issue with footer className

--- whitakr-portfolio/src/components/Footer.tsx
+++ whitakr-portfolio/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
 	return (
-		<footer className>
+		<footer className="mt-8">
 			<p className>Here's the footer with some various info that may be important or may not be</p>
 		</footer>
 	);
