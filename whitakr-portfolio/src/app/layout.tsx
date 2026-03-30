bc254280f6237644444852cd5b3d981251e744f4
Whitaker Trebella <wtrebella@gmail.com>
2026-03-30 15:07:58 -0700

    Open graph icon

--- whitakr-portfolio/src/app/layout.tsx
+++ whitakr-portfolio/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Roboto_Slab } from "next/font/google";
 import "./globals.css";
 import React from "react";
-import Footer from "@/components/Footer";
+{/*import Footer from "@/components/Footer";*/}
 import Header from "@/components/Header";
 import PageBackground from "@/components/PageBackground";
 
@@ -10,9 +10,24 @@ const robotoSlab = Roboto_Slab({
     subsets: ["latin"],
 });
 
+const titleText = "Whitaker Trebella"
+const descriptionText =  "Portfolio of games, apps, music, and websites"
+
 export const metadata: Metadata = {
-    title: "Whitaker Trebella",
-    description: "Portfolio of games, apps, music, and websites",
+    title: titleText,
+    description: descriptionText,
+    openGraph: {
+        title: titleText,
+        description: descriptionText,
+        images: [
+            {
+                url: "/icon-open-graph.jpg",
+                width: 256,
+                height: 256,
+                alt: "Whitaker Trebella"
+            }
+        ]
+    }
     icons: {
         icon: "/icon.png"
     }
